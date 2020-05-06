c85b2322c93bbda789e4fd39dbefa66a10c851be
chaips <ar0439708@gmail.com>
2020-05-06 13:06:44 -0500

    Creación de la vista del inicio de sesión

---
+++ app/src/main/java/com/example/cbooks/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.cbooks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.cbooks.login.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        showFragment(LoginFragment())
+    }
+
+     private fun showFragment(fragment : Fragment){
+         supportFragmentManager.beginTransaction().replace(R.id.container, fragment)
+             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+             .commit()
+    }
+}
